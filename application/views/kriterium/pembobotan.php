6785580102ed4eada4a4e6a85c9b7e7b59cc299c
Ajenkris Kungkung <kristt26@gmail.com>
2023-04-04 11:53:23 +0900

    sd

--- application/views/kriterium/pembobotan.php
+++ application/views/kriterium/pembobotan.php
@@ -3,67 +3,83 @@
         <div class="box">
             <div class="box-header">
                 <h3 class="box-title">Bobot Kriteria</h3>
-            	<!-- <div class="box-tools">
+                <!-- <div class="box-tools">
                     <a href="<?php echo site_url('kriterium/add'); ?>" class="btn btn-success btn-sm">Add</a>
                 </div> -->
             </div>
             <div class="box-body">
                 <table class="table table-hover">
-                    <tr ng-repeat = "item in element">
+                    <tr ng-repeat="item in element">
                         <td class="align-middle" style="width:20%">{{item.kriteria.kriteria}}</td>
                         <td>
                             <table class="table">
                                 <tr>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.11111111111111">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.11111111111111">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.125">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.125">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.14285714285714">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.14285714285714">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.16666666666667">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.16666666666667">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.2">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.2">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.25">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.25">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.33333333333333">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.33333333333333">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="0.5">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="0.5">
                                     </td>
                                     <td>
                                         =
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="2">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="2">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="3">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="3">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="4">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="4">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="5">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="5">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="6">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="6">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="7">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="7">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="8">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="8">
                                     </td>
                                     <td>
-                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1" ng-model="item.bobot" ng-change="setNilai(item)" value="9">
+                                        <input type="radio" name="optionsRadios{{$index+1}}" id="optionsRadios1"
+                                            ng-model="item.bobot" ng-change="setNilai(item)" value="9">
                                     </td>
                                 </tr>
                                 <tr>
@@ -121,7 +137,7 @@
                                 </tr>
                             </table>
                         </td>
-                        <td class="align-middle text-right"  style="width:20%">{{item.kriteria1.kriteria}}</td>
+                        <td class="align-middle text-right" style="width:20%">{{item.kriteria1.kriteria}}</td>
                     </tr>
                 </table>
                 <div>
@@ -133,77 +149,77 @@
         </div>
     </div>
     <div class="col-md-12" ng-show="datas.CR">
-            <div class="col-md-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Bobot Kriteria</h3>
-                        <!-- <div class="box-tools">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Bobot Kriteria</h3>
+                    <!-- <div class="box-tools">
                             <a href="<?php echo site_url('kriterium/add'); ?>" class="btn btn-success btn-sm">Add</a>
                         </div> -->
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <td></td>
-                                    <td ng-repeat="item in datas.criterias">
-                                        {{item.name}}
-                                    </td>
-                                </tr>
-                            </thead>
-                            <tbody ng-repeat="item in datas.criterias">
-                                <tr>
-                                    <td>
-                                        {{item.name}}
-                                    </td>
-                                    <td ng-repeat="bobot in datas.bobomatrix[$index]">
-                                        {{bobot | limitTo:5}}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                </div>
+                <div class="box-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td></td>
+                                <td ng-repeat="item in datas.criterias">
+                                    {{item.name}}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody ng-repeat="item in datas.criterias">
+                            <tr>
+                                <td>
+                                    {{item.name}}
+                                </td>
+                                <td ng-repeat="bobot in datas.bobomatrix[$index]">
+                                    {{bobot | limitTo:5}}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="col-md-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Matriks Normal</h3>
-                        <!-- <div class="box-tools">
+        </div>
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Matriks Normal</h3>
+                    <!-- <div class="box-tools">
                             <a href="<?php echo site_url('kriterium/add'); ?>" class="btn btn-success btn-sm">Add</a>
                         </div> -->
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <td></td>
-                                    <td ng-repeat="item in datas.relativecriteria">
-                                        {{item.name}}
-                                    </td>
-                                </tr>
-                            </thead>
-                            <tbody ng-repeat="item in datas.criterias">
-                                <tr>
-                                    <td>
-                                        {{item.name}}
-                                    </td>
-                                    <td ng-repeat="bobot in datas.matrixnormal[$index]">
-                                        {{bobot | limitTo:5}}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                </div>
+                <div class="box-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td></td>
+                                <td ng-repeat="item in datas.relativecriteria">
+                                    {{item.name}}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody ng-repeat="item in datas.criterias">
+                            <tr>
+                                <td>
+                                    {{item.name}}
+                                </td>
+                                <td ng-repeat="bobot in datas.matrixnormal[$index]">
+                                    {{bobot | limitTo:5}}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
     </div>
     <div class="col-md-12">
         <div class="alert alert-success alert-dismissible" ng-show="datas.CR<0.1">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <h4><i class="icon fa fa-check"></i>Nilai CR: {{datas.CR}} (Konsisten)</h4>
-              </div>
-        <div class="alert alert-danger alert-dismissible"  ng-show="datas.CR>0.1">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h4><i class="icon fa fa-check"></i>Nilai CR: {{datas.CR}} (Konsisten)</h4>
+        </div>
+        <div class="alert alert-danger alert-dismissible" ng-show="datas.CR>0.1">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <h4><i class="icon fa fa-ban"></i>Nilai CR: {{datas.CR}} (Tidak Konsisten)</h4>
         </div>
